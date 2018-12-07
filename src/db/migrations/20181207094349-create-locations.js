8f0100786c0115b3842c13e54e611771adde340a
Rex Ogbemudia <rex.ogbemudia@andela.com>
2018-12-07 15:35:03 +0100

    create model

---
+++ src/db/migrations/20181207094349-create-locations.js
@@ -0,0 +1,53 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Locations', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: Sequelize.STRING,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    maleResidents: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    femaleResidents: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    totalResidents: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    parentLocation: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      references: {
+        model: 'Locations',
+        key: 'id',
+      },
+    },
+    childLocation: {
+      type: Sequelize.STRING,
+      allowNull: true,
+      references: {
+        model: 'Locations',
+        key: 'id',
+      },
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('Locations'),
+};
---
+++ src/db/models/locations.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, DataTypes) => {
+  const Locations = sequelize.define(
+    'Locations',
+    {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.STRING,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      maleResidents: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      femaleResidents: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      totalResidents: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      parentLocation: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      childLocation: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+    },
+    {},
+  );
+  Locations.associate = function association(models) {
+    // associations can be defined here
+    Locations.hasMany(models.Locations, { as: 'childLocations' });
+    Locations.belongsTo(models.Locations, { as: 'parentLocation' });
+  };
+  return Locations;
+};
